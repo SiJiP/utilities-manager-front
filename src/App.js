b2c2946e6465058f75fb0c8012cb19cbf8e6381a
Serhii Petruniv <spetru@softserveinc.com>
2020-06-07 19:23:24 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit<code>src/App.js</code> and save to reload.
-          I'm done with CI
+          I'm done with CI....
         </p>
         <a
           className="App-link"
